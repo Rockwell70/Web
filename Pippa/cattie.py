c126429e9438b4f5d098dd3d5e31c9e3e40dc56f
JRockwell70 <rockwellshabani@gmail.com>
2019-07-22 17:15:36 -0700

    added image

--- Pippa/cattie.py
+++ Pippa/cattie.py
@@ -3,15 +3,18 @@ import os
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
-#app.secret_key = b"\x9f\x00\x1d.i\x80U\x8c\x9bPD]\xfbu\xe2\x8d\x12\x12\xc5\xef]h\xb0Z"
-app.secret_key = os.environ.get('SECRET_KEY').encode()
+app.secret_key = b"\x9f\x00\x1d.i\x80U\x8c\x9bPD]\xfbu\xe2\x8d\x12\x12\xc5\xef]h\xb0Z"
+#app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 @app.route('/')
 @app.route('/index')
 def intro():
     body = """
-        <h1>Hello Pippa</h>
+        <h1>Hello Pippa!</h>
         <li><img src="https://www.wideopenspaces.com/wp-content/uploads/2018/04/ftd-babyrabbit.jpg" class="card-img-top" alt="Bunnies"></li>
+        <h2>Here's a kitty!</h2>
+        <li><img src="http://images6.fanpop.com/image/photos/34800000/Kittens-3-animals-34865316-1920-1200.jpg" 
+        class="card-img-top" alt="Kitties width="600" height="450""></li>
         """
 
     return Response(response=body, mimetype="text/html")
